afba8f29beadffe57cc57bde3d70325b8d9ac238
Simon <sai@gmail.com>
2025-09-06 16:51:54 -0400

    Second Commit

--- .idea/Springboot1/Helloworld.java
+++ .idea/Springboot1/Helloworld.java
@@ -3,5 +3,9 @@ package Springboot1;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println(":HJellohadsfn");
+        int abc=18;
+        int ccc=324;
+        int ddd=abc+ccc;
+        System.out.println(ddd);
     }
 }
